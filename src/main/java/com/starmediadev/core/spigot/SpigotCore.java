1947745c9de1d0f678bd31abf757742ed6c74cbd
Firestar311 <firestar311mc@gmail.com>
2021-06-24 20:41:57 -0500

    Changed how the database stuff is loaded

--- src/main/java/com/starmediadev/core/spigot/SpigotCore.java
+++ src/main/java/com/starmediadev/core/spigot/SpigotCore.java
@@ -1,10 +1,9 @@
 package com.starmediadev.core.spigot;
 
+import com.starmediadev.data.Context;
+import com.starmediadev.data.StarData;
 import com.starmediadev.data.manager.DatabaseManager;
-import com.starmediadev.data.manager.SingleDatabaseManager;
 import com.starmediadev.data.properties.SqlProperties;
-import com.starmediadev.data.registries.DataObjectRegistry;
-import com.starmediadev.data.registries.TypeRegistry;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.ServicePriority;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -16,9 +15,7 @@ public class SpigotCore extends JavaPlugin {
     public void onEnable() {
         this.saveDefaultConfig();
 
-        TypeRegistry typeRegistry = TypeRegistry.createInstance(getLogger());
-        DataObjectRegistry dataObjectRegistry = DataObjectRegistry.createInstance(getLogger(), typeRegistry);
-        this.databaseManager = new SingleDatabaseManager(getLogger(), dataObjectRegistry, typeRegistry);
+        this.databaseManager = new StarData(Context.SINGLE, getLogger()).getDatabaseManager();
         this.databaseManager.createDatabase(new SqlProperties().setDatabase(getConfig().getString("mysql.database")).setHost(getConfig().getString("mysql.host"))
         .setPassword(getConfig().getString("mysql.password")).setPort(getConfig().getInt("mysql.port")).setUsername(getConfig().getString("mysql.username")));
         Bukkit.getServicesManager().register(DatabaseManager.class, this.databaseManager, this, ServicePriority.Highest);
