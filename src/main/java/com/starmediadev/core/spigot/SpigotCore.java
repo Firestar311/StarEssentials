6efb1163149750afa2f8ff5a1533896ec1f23f85
Firestar311 <slaterandrew0398@gmail.com>
2021-08-20 21:59:42 -0500

    Updated missed names for maven to make it consistent

--- src/main/java/com/starmediadev/core/spigot/SpigotCore.java
+++ src/main/java/com/starmediadev/core/spigot/SpigotCore.java
@@ -1,11 +1,6 @@
 package com.starmediadev.core.spigot;
 
-import com.starmediadev.data.Context;
-import com.starmediadev.data.StarData;
 import com.starmediadev.data.manager.DatabaseManager;
-import com.starmediadev.data.properties.SqlProperties;
-import org.bukkit.Bukkit;
-import org.bukkit.plugin.ServicePriority;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class SpigotCore extends JavaPlugin {
@@ -14,10 +9,5 @@ public class SpigotCore extends JavaPlugin {
     
     public void onEnable() {
         this.saveDefaultConfig();
-
-        this.databaseManager = new StarData(Context.SINGLE, getLogger()).getDatabaseManager();
-        this.databaseManager.setupDatabase(new SqlProperties().setDatabase(getConfig().getString("mysql.database")).setHost(getConfig().getString("mysql.host"))
-        .setPassword(getConfig().getString("mysql.password")).setPort(getConfig().getInt("mysql.port")).setUsername(getConfig().getString("mysql.username")));
-        Bukkit.getServicesManager().register(DatabaseManager.class, this.databaseManager, this, ServicePriority.Highest);
     }
 }
