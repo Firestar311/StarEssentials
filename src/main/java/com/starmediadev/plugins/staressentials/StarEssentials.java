a1437a1e6cf651b01f303964b73fa9e225099b13
Firestar311 <slaterandrew0398@gmail.com>
2022-02-02 04:40:39 -0600

    Renamed to StarEssentials (will rename repo soon)

--- src/main/java/com/starmediadev/plugins/staressentials/StarEssentials.java
+++ src/main/java/com/starmediadev/plugins/staressentials/StarEssentials.java
@@ -1,8 +1,8 @@
-package com.starmediadev.plugins.spigotcore;
+package com.starmediadev.plugins.staressentials;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class SpigotCore extends JavaPlugin {
+public class StarEssentials extends JavaPlugin {
 
     /*
     Add the following commands/features
