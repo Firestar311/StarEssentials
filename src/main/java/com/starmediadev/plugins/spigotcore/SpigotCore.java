cee05905e8eef9811c0217879b0b1b4208fd013b
Firestar311 <slaterandrew0398@gmail.com>
2022-02-02 04:13:31 -0600

    Added more todos

--- src/main/java/com/starmediadev/plugins/spigotcore/SpigotCore.java
+++ src/main/java/com/starmediadev/plugins/spigotcore/SpigotCore.java
@@ -17,10 +17,38 @@ public class SpigotCore extends JavaPlugin {
     god command
     heal command
     kill command
-    messaging commands (eventually add support for detecting StarChat)
-    Nick command (Just the name)
     spawn features (spawn itself, setting spawn, teleporting players on first login to spawn, teleporting players to spawn always (configurable)
+    editsign
+    repair
+    weather
+    time
+    homes
+    warps
+    enderchest
+    trash
+    top (go to highest block at your location)
+    spawnmob
+    more (add more items based on what is holding)
+    List (Add an API for this though and can make it based on the ranks)
+    Item command (Spawn an item, will have support for StarItems, but does not replace this command)
+    near
+    enchant
+    workbench
+    Give
+    Sudo (Add a hook for StarPerms/API for StarPerms to allow more control over this)
+    Back
+    
+    Stuff to add here and add checks for the other more indepth plugins to disable
+    spawner (change spawner type)
+    invsee (and echest variant) will be in a moderator tools plugin eventually, this provides a very basic thing
+    basic punishment commands (kick, warn, ban, tempban, mute, tempmute and kickall)
+    Nicknames
+    messaging commands (eventually add support for detecting StarChat), also toggle commands
     tp commands (tp, tpall, tphere, tpa, tpahere)
+    Very basic economy system with Vault integration
+    Ignore (StarChat overrides this)
+    Socialspy
+    Vanish
      */
     
     public void onEnable() {
