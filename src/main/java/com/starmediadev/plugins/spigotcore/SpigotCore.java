64670d02fb59cf04218619505938a6ba14faf2ae
Firestar311 <slaterandrew0398@gmail.com>
2022-02-02 04:01:25 -0600

    Added a todo list in a comment in the main class

--- src/main/java/com/starmediadev/plugins/spigotcore/SpigotCore.java
+++ src/main/java/com/starmediadev/plugins/spigotcore/SpigotCore.java
@@ -1,11 +1,27 @@
 package com.starmediadev.plugins.spigotcore;
 
-import com.starmediadev.data.manager.DatabaseManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class SpigotCore extends JavaPlugin {
 
-    private DatabaseManager databaseManager;
+    /*
+    Add the following commands/features
+    
+    Broadcast Command
+    ClearInv command
+    Feed Command
+    Fly Command
+    flyspeed command
+    walkspeed command
+    gamemode command (With specific shortcuts)
+    god command
+    heal command
+    kill command
+    messaging commands (eventually add support for detecting StarChat)
+    Nick command (Just the name)
+    spawn features (spawn itself, setting spawn, teleporting players on first login to spawn, teleporting players to spawn always (configurable)
+    tp commands (tp, tpall, tphere, tpa, tpahere)
+     */
     
     public void onEnable() {
         this.saveDefaultConfig();
